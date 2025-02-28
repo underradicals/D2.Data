24090b4ef488b110c30426f4cfb4c049fb6443d1
Joseph <underradicals@gmail.com>
2025-02-27 18:35:43 -0600

    Add gitignore

---
+++ D2.Data.Prototype/Common/app_settings.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from json import loads
+
+ROOT = Path().cwd()
+APP_SETTINGS = ROOT / "appsettings.json"
+
+
+class Auth:
+    SECTION_NAME = "Auth"
+
+    def __init__(self, apikey: str, baseurl: str):
+        self.apikey = apikey
+        self.baseurl = baseurl
+
+
+def get_config(section_name) -> Auth:
+    with open(APP_SETTINGS, "r", encoding="utf-8") as file:
+        data = loads(file.read())
+
+    match section_name:
+        case "Auth":
+            auth = Auth(data[section_name]["apikey"], data[section_name]["baseurl"])
+            return auth
+        case _:
+            print("Default")
+
+
+if __name__ == "__main__":
+    pass
