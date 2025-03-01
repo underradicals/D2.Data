c6f7c4ffbd0286e1177dbeec1d018deb2bd0c694
Joseph <underradicals@gmail.com>
2025-02-28 18:33:05 -0600

    Add Tests    Add test for appsettings    Add test for file paths

---
+++ D2.Data.Prototype/tests/Common/test_app_settings.py
@@ -0,0 +1,17 @@
+﻿from Common import get_config, Auth, APP_SETTINGS
+
+def test_get_config() -> None:
+    # Arrange
+    config = get_config(Auth.SECTION_NAME)
+
+    # Act
+    apikey = config.apikey
+    baseurl = config.baseurl
+
+    # Asert
+    assert apikey is not None
+    assert baseurl is not None
+    assert apikey != ""
+    assert baseurl != ""
+
+
